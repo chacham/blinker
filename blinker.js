f1894adff0fbc07df1c5a6ea3ff4ff673a99a8f8
chacham <ccm0702@naver.com>
2020-11-01 22:12:34 +0900

    Refactor with vue and add color selector
    Resolve #1

--- blinker.js
+++ blinker.js
@@ -1,42 +1,32 @@
-class Blinker {
-    constructor() {
-        this.durations = [100, 100];
-        this.colors = ['black', 'white'];
-        this.bodyElement = document.querySelector("body");
-    }
-
-    runBlink() {
-        if (!Number(this.durations.length) || Number(this.durations.length) <= 0) {
-            console.error('Invalid durations');
-            return;
-        }
-        this.blinkTimeoutLoop(0);
-    }
-
-    setBgColor(index) {
-        const curColor = this.colors[index % this.colors.length];
-        this.bodyElement.style.background = curColor;
-    }
-
-    blinkTimeoutLoop(index) {
-        this.setBgColor(index);
-        const duration = this.durations[index];
-        setTimeout(() => {
-            const nextIndex = (index + 1) % this.durations.length;
-            this.blinkTimeoutLoop(nextIndex);
-        }, duration);
-    }
-}
-
-const blinker = new Blinker();
-
-function updateDuration() {
-    const durations = [
-        Number(document.querySelector("#duration-input-01").value) || 0,
-        Number(document.querySelector("#duration-input-02").value) || 0,
-    ];
-    blinker.durations = durations;
-}
-
-updateDuration()
-blinker.runBlink();
+const app = new Vue({
+    el: '#app',
+    data: {
+        currentIndex: 0,
+        currentColor: 'white',
+        items: [
+            { duration: 3000, color: '#000000' },
+            { duration: 200, color: '#FFFFFF' },
+        ],
+    },
+    methods: {
+        getIndexTitle(index) {
+            if ([11, 12, 13].includes(index)) return `${index}th!`;
+            const last = index % 10;
+            if (last == 1) return `${index}st!`;
+            if (last == 2) return `${index}nd!`;
+            if (last == 3) return `${index}rd!`;
+            return `${index}th!`
+        },
+        blink() {
+            const currentItem = this.items[this.currentIndex];
+            this.currentColor = currentItem.color;
+            setTimeout(() => {
+                this.currentIndex = (this.currentIndex + 1) % this.items.length;
+                this.blink();
+            }, currentItem.duration);
+        },
+    },
+    mounted() {
+        this.blink();
+    },
+})
